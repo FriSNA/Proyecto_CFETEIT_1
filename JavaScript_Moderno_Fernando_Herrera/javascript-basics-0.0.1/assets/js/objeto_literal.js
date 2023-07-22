2f983b1307a661017b2043da411e55b99ec54ed6
Frida Sofía Nieto Ambriz <frida.cfeteit@gmail.com>
2023-07-21 19:48:19 -0600

    Fernando herrera: Objetos literales

--- JavaScript_Moderno_Fernando_Herrera/javascript-basics-0.0.1/assets/js/objeto_literal.js
+++ JavaScript_Moderno_Fernando_Herrera/javascript-basics-0.0.1/assets/js/objeto_literal.js
@@ -1,4 +1,15 @@
 let personaje = {
 
-    nombre: 'Sofía Nieto'
+    nombre: 'Sofía Nieto',
+    codeName: 'Frida',
+    vivo: false,
+    edad: 50,
+    coords: {
+        Lat: 34.034,
+        Lon: -118.70
+    },
+    trajes: ['Bióloga', 'Civil', 'Deportivo'] ,
 };
+
+console.log('Nombre: ', personaje);
+ console.log('Nombre: ', personaje.nombre);
